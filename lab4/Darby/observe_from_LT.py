bfa8c976c6036910bd501c56e6e3488eb4e97f29
Darby McCauley <darbymccauley@berkeley.edu>
2022-04-14 11:39:27 -0700

    fixed printing errors

--- lab4/Darby/observe_from_LT.py
+++ lab4/Darby/observe_from_LT.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
 
 import ugradio
 
@@ -14,11 +12,9 @@ from astropy import units as u
 from astropy.time import Time
 from astropy.coordinates import SkyCoord, AltAz, EarthLocation
 
+# ------------------------------------------------------------------------------------------------------------------------------------------
 
-# In[2]:
-
-
-def take_leusch_data(init_l, init_b, nspec):
+def take_leusch_data(init_l, init_b, nspec, filename_noise_on, filename_noise_off):
     
     """
     Take data using the Leuschner Telescope at Leuschner Observatory.
@@ -27,10 +23,15 @@ def take_leusch_data(init_l, init_b, nspec):
         init_l: galactic longitude(s)
         init_b: galactic latitude(s)
         nspec: number of spectra to collect at each coordinate point 
-            Each iteration takes ~0.7s, so nspec = 1 will take approxmately 700 seconds to run.a
+            Each iteration takes ~0.7s, so nspec = 1 will take approxmately 700 seconds to run
+        filename_noise_on: name of the file that will collect data with the noise diode on. Input as a string, ending with '.fits' so that it saves as a FITS file.
+            Example: filename_noise_on = 'file_with_noise_on.fits'
+        filename_noise_off: name of the file that will collect data with the noise diode off. Input as a string, ending with '.fits' so that it saves as a FITS file.
+            Example: filename_noise_off = 'file_with_noise_off.fits'
+            
                 
     Returns:
-        Two FITS files, one in which the noise diode was one and another in which the noise diode was off.
+        Two FITS files, one in which the noise diode was one and another in which the noise diode was off, named according to input strings.
     """
     
     #-------------------------------------------------------------------------------------------------------
@@ -40,7 +41,6 @@ def take_leusch_data(init_l, init_b, nspec):
     spectrometer = ugradio.leusch.Spectrometer()
     noise = ugradio.leusch.LeuschNoise()
     
-    telescope.stow() # stow to initialize data-taking
     
     LT_lat, LT_lon, LT_alt = ugradio.leo.lat, ugradio.leo.lon, ugradio.leo.alt # get LT coords
     
@@ -66,22 +66,36 @@ def take_leusch_data(init_l, init_b, nspec):
     end_obs = False
     for l in init_l:
         for b in init_b:
-            alt, az = calc_pos() #calculate alt, az
+            alt, az = calc_pos() #calculate alt, az,
             print('Moving to position... \n')
+            
+            
             try:
                 telescope.point(alt, az) # point LT to calculated alt, az
-                print('Current LT alt, az (degrees): {0:.4f}, {1:.4f}'.format(telescope.get_pointing()))
+                
+                print('Current L.T. alt, az (degrees): {0:.4f}, {1:.4f}'.format(alt, az), 
+                      '\nTurning on noise diode.)
                 noise.on()
-                spectrometer.read_spec('noise_on.fits', nspec, (l,b), 'ga')
+                print('Collecting spectrum with noise diode on.')
+                spectrometer.read_spec(filename_noise_on, nspec, (l,b), 'ga')
+                      
+                print('Finished collecting spectrum with noide diode on. Turning off noise diode...')
                 noise.off()
-                spectrometer.read_spec('noise_off.fits', nspec, (l,b), 'ga')
+                print('Collecting spectrum with noise diode off.')
+                spectrometer.read_spec(filename_noise_off, nspec, (l,b), 'ga')
+                print('Finished collecting spectrum with noise diode off.')
+                      
+                      
             except: # If pointing fails...
-                print('Failed to point to (alt, az)=({0:.4f}, {1:.4f}) (degrees)'.format(telescope.get_pointing()), '\nPointing to next (alt, az).')
+                print('Failed to point to (alt, az)=({0:.4f}, {1:.4f}) (degrees)'.format(alt, az), 
+                      '\nPointing to next (alt, az).')
                 n_fails += 1
                 if n_fails == 10: # If number of pointing errors = 10 then break and end obs
                     end_obs = True
                     break
                 continue
+                      
+                      
         if end_obs:
             print('Too many pointing errors... Ending observation.')
             break # end the observation
@@ -90,15 +104,7 @@ def take_leusch_data(init_l, init_b, nspec):
     telescope.stow() # stow when finished
                 
 
+# ------------------------------------------------------------------------------------------------------------------------------------------
 
-# In[4]:
-
-
-# take_leusch_data(120, 0, 1000)
-
-
-# In[ ]:
-
-
-
+#take_leusch_data(init_l, init_b, nspec, filename_noise_on, filename_noise_off)
 
