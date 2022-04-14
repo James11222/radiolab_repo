60b080dafd9e8321dd2e15e78a91d5a5f27b1514
Darby McCauley <darbymccauley@berkeley.edu>
2022-04-14 13:39:49 -0700

    fixed for loops

--- lab4/Darby/observe_from_LT.py
+++ lab4/Darby/observe_from_LT.py
@@ -48,15 +48,13 @@ def take_leusch_data(init_l, init_b, nspec, filename_noise_on, filename_noise_of
     #-------------------------------------------------------------------------------------------------------
     #                      Define function that transforms between coordinate systems            
     #-------------------------------------------------------------------------------------------------------
-    def calc_pos():
-        for l in init_l:
-            for b in init_b:
-                from astropy.coordinates import SkyCoord, AltAz, EarthLocation
-                gc = SkyCoord(l=l*u.degree, b=b*u.degree, frame='galactic')
-                loc = EarthLocation(lat=LT_lat*u.deg, lon=LT_lon*u.deg, height=LT_alt*u.m)
-                time = Time(ugradio.timing.utc(fmt='%Y-%m-%d %X'))
-                AltAz = gc.transform_to(AltAz(obstime=time, location=loc))
-                alt, az = AltAz.alt.degree, AltAz.az.degree
+    def calc_pos(l, b):
+            from astropy.coordinates import SkyCoord, AltAz, EarthLocation
+            gc = SkyCoord(l=l*u.degree, b=b*u.degree, frame='galactic')
+            loc = EarthLocation(lat=LT_lat*u.deg, lon=LT_lon*u.deg, height=LT_alt*u.m)
+            time = Time(ugradio.timing.utc(fmt='%Y-%m-%d %X'))
+            AltAz = gc.transform_to(AltAz(obstime=time, location=loc))
+            alt, az = AltAz.alt.degree, AltAz.az.degree
         return alt, az
     
     #-------------------------------------------------------------------------------------------------------
@@ -64,9 +62,10 @@ def take_leusch_data(init_l, init_b, nspec, filename_noise_on, filename_noise_of
     #-------------------------------------------------------------------------------------------------------
     n_fails = 0
     end_obs = False
+    
     for l in init_l:
         for b in init_b:
-            alt, az = calc_pos() #calculate alt, az,
+            alt, az = calc_pos(l, b) #calculate alt, az,
             print('Moving to position... \n')
             
             
@@ -77,12 +76,12 @@ def take_leusch_data(init_l, init_b, nspec, filename_noise_on, filename_noise_of
                       '\nTurning on noise diode.)
                 noise.on()
                 print('Collecting spectrum with noise diode on.')
-                spectrometer.read_spec(filename_noise_on, nspec, (l,b), 'ga')
+                spectrometer.read_spec(filename_noise_on+'_'+str((l, b)), nspec, (l,b), 'ga')
                       
                 print('Finished collecting spectrum with noide diode on. Turning off noise diode...')
                 noise.off()
                 print('Collecting spectrum with noise diode off.')
-                spectrometer.read_spec(filename_noise_off, nspec, (l,b), 'ga')
+                spectrometer.read_spec(filename_noise_off+'_'+str((l, b)), nspec, (l,b), 'ga')
                 print('Finished collecting spectrum with noise diode off.')
                       
                       
